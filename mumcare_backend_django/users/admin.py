af81cf88210d2bd23c718dff71d42d4bbb5d0a4b
deno101 <dennizwarui@gmail.com>
2020-08-28 13:06:37 +0300

    personalize admin

--- mumcare_backend_django/users/admin.py
+++ mumcare_backend_django/users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from users.models import (
+    CustomUser,
+    Role,
+    Appointment
+)
+
 
 # Register your models here.
+class RoleAdmin(admin.ModelAdmin):
+    fields = ('role_name',)
+    list_display = ('id', 'role_name')
+
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ('first_name', 'last_name', 'location', 'phone_number', 'role')
+    list_display = ('first_name', 'last_name', 'location', 'phone_number', 'role', 'username')
+
+
+admin.site.register(Role, RoleAdmin)
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Appointment)
--- mumcare_backend_django/users/models.py
+++ mumcare_backend_django/users/models.py
@@ -1,29 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
 from core_app.utils.utils import *
-from django.contrib import admin
 
 
 # Create your models here.
 
+def gen():
+    return generate_code(n=100)
+
+
 class CustomUser(User):
     role = models.ForeignKey('Role', on_delete=models.CASCADE)
-    session_id = models.CharField(max_length=100, default='', unique=True)
-    phone_number = models.CharField(max_length=10, unique=True)
+    session_id = models.CharField(max_length=100, default=gen, unique=True)
+    phone_number = models.CharField(max_length=10, unique=True, blank=False)
     location = models.CharField(max_length=100)
+
+    # username = models.CharField(max_length=100, unique=False)
 
     def has_role(self, role: str):
         return self.role.role_name == role
 
+    def __str__(self):
+        return self.first_name + '_' + self.last_name
 
-admin.site.register(CustomUser)
+    def save(self, *args, **kwargs):
+        try:
+            self.username = self.first_name + "_" + self.last_name + generate_code(n=10)
+        except Exception:
+            self.username = ""
+        super(CustomUser, self).save(*args, **kwargs)
 
 
 class Role(models.Model):
     role_name = models.CharField(max_length=100, null=False, blank=False)
 
+    def __str__(self):
+        return self.role_name
 
-admin.site.register(Role)
 
 APPOINTMENT_STATUS = [
     ('N', 'None'),
@@ -40,9 +53,6 @@ class Appointment(models.Model):
         max_length=10,
         choices=APPOINTMENT_STATUS,
         default='N')
-
-
-admin.site.register(Appointment)
 
 
 class AuthenticationCodes(models.Model):
